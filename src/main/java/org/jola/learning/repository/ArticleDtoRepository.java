ab51dbf957c6740bb124b8059aa422dcfa94e429
GitUserJoLa <josephine.langer@gmx.de>
2024-06-11 14:32:35 +0200

    removed redundant public modifiers from interface members

--- src/main/java/org/jola/learning/repository/ArticleDtoRepository.java
+++ src/main/java/org/jola/learning/repository/ArticleDtoRepository.java
@@ -7,12 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ArticleDtoRepository extends CrudRepository<ArticleDto, Long> {
-    public Optional<ArticleDto> findByTitle(String title);
-    public List<ArticleDto> findByReadCount(Long readCount);
-    public List<ArticleDto> findByHashTags(String[] hashTags);
-    public List<ArticleDto> findByAuthorId(Long id);
-    public List<ArticleDto> findByAuthorAlias(String alias);
-    public List<ArticleDto> findByAuthorLastName(String lastName);
-    public List<ArticleDto> findByAuthorFirstName(String firstName);
-
+    Optional<ArticleDto> findByTitle(String title);
+    List<ArticleDto> findByReadCount(Long readCount);
+    List<ArticleDto> findByHashTags(String[] hashTags);
+    List<ArticleDto> findByAuthorId(Long id);
+    List<ArticleDto> findByAuthorAlias(String alias);
+    List<ArticleDto> findByAuthorLastName(String lastName);
+    List<ArticleDto> findByAuthorFirstName(String firstName);
 }
