ad8c85326687d754fe46dcca9db3e0994d306a54
GitUserJoLa <josephine.langer@gmx.de>
2024-06-11 11:55:25 +0200

    updated repository with custom find functions

--- src/main/java/org/jola/learning/repository/ArticleDtoRepository.java
+++ src/main/java/org/jola/learning/repository/ArticleDtoRepository.java
@@ -4,8 +4,15 @@ import org.jola.learning.dto.ArticleDto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ArticleDtoRepository extends CrudRepository<ArticleDto, Long> {
-    public ArticleDto findByTitle(String title);
-    public List<ArticleDto> findByAuthorDtoAuthor(Long id);
+    public Optional<ArticleDto> findByTitle(String title);
+    public List<ArticleDto> findByReadCount(Long readCount);
+    public List<ArticleDto> findByHashTags(String[] hashTags);
+    public List<ArticleDto> findByAuthorId(Long id);
+    public List<ArticleDto> findByAuthorAlias(String alias);
+    public List<ArticleDto> findByAuthorLastName(String lastName);
+    public List<ArticleDto> findByAuthorFirstName(String firstName);
+
 }
