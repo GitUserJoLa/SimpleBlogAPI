6444e7e68f655dc3b3d3540679f85fed0b689bec
GitUserJoLa <josephine.langer@gmx.de>
2024-06-13 10:51:25 +0200

    modified method getByAlias to use lambda expression instead of if-else

--- src/main/java/org/jola/learning/controller/AuthorController.java
+++ src/main/java/org/jola/learning/controller/AuthorController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,9 +27,20 @@ public class AuthorController {
     }
 
     @GetMapping(value = "/authors/{alias}")
-    public ResponseEntity<AuthorDto> getAuthorByAlias(@PathVariable String alias){
+    public ResponseEntity<AuthorDto> getAuthorByAlias(
+            @PathVariable String alias
+//            ,
+//            @RequestParam(name = "age") String age,
+//            @RequestParam(name = "gender", required = false) String gender
+    ){
+//        System.out.println("Age: " + age + ", Gender: " + gender);
         Optional<AuthorDto> author = authorService.getAuthorByAlias(alias);
-        if(author.isPresent()) return new ResponseEntity<>(author.get(), HttpStatus.OK);
-        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        // map()-idiom is used with lists and list-like containers
+        return author.map(
+                authorDto -> new ResponseEntity<>(authorDto, HttpStatus.OK)
+                ).
+                orElseGet(
+                        () -> new ResponseEntity<>(HttpStatus.NOT_FOUND)
+                );
     }
 }
