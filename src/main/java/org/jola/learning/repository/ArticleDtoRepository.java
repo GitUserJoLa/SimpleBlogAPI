5b5778139609e33086f92767acee9936756d668a
GitUserJoLa <josephine.langer@gmx.de>
2024-06-21 12:59:26 +0200

    bugfix: changed repo method retun type

--- src/main/java/org/jola/learning/repository/ArticleDtoRepository.java
+++ src/main/java/org/jola/learning/repository/ArticleDtoRepository.java
@@ -4,16 +4,15 @@ import org.jola.learning.dto.ArticleDto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ArticleDtoRepository extends CrudRepository<ArticleDto, Long> {
-    Optional<ArticleDto> findByTitleAllIgnoreCase(String title);
+    List<ArticleDto> findByTitleIgnoreCase(String title);
     List<ArticleDto> findByReadCount(Long readCount);
 //    List<ArticleDto> findByHashTags(String[] hashTags);
     List<ArticleDto> findByAuthorId(Long id);
-    List<ArticleDto> findByAuthorAliasAllIgnoreCase(String alias);
-    List<ArticleDto> findByAuthorLastNameAllIgnoreCase(String lastName);
-    List<ArticleDto> findByAuthorFirstNameAllIgnoreCase(String firstName);
+    List<ArticleDto> findByAuthorAliasIgnoreCase(String alias);
+    List<ArticleDto> findByAuthorLastNameIgnoreCase(String lastName);
+    List<ArticleDto> findByAuthorFirstNameIgnoreCase(String firstName);
     List<ArticleDto> findByAuthorFirstNameAndAuthorLastNameAllIgnoreCase(String firstName, String lastName);
 
 }
