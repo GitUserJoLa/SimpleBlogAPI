bf5d3f06d77bad7fb9fa35d253a32ce99f4414a7
GitUserJoLa <josephine.langer@gmx.de>
2024-06-22 16:47:39 +0200

    add response creation method to avoid boilerplate code and overhaul mappings

--- src/main/java/org/jola/learning/controller/AuthorController.java
+++ src/main/java/org/jola/learning/controller/AuthorController.java
@@ -1,15 +1,14 @@
 package org.jola.learning.controller;
 
+import org.jola.learning.dto.ArticleDto;
 import org.jola.learning.dto.AuthorDto;
 import org.jola.learning.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class AuthorController {
@@ -23,33 +22,21 @@ public class AuthorController {
             @RequestParam(name = "firstname", required = false) String firstName
     ) {
         List<AuthorDto> authorList = authorService.getAuthors(lastName, firstName);
-
-        if (authorList.isEmpty())
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        else
-            return new ResponseEntity<>(authorList, HttpStatus.OK);
+        return createResponseEntity(authorList);
     }
 
     @GetMapping(value = "/authors/{alias}")
     public ResponseEntity<List<AuthorDto>> getAuthorByAlias(@PathVariable String alias) {
 
         List<AuthorDto> authorList = authorService.getAuthorByAlias(alias);
+        return createResponseEntity(authorList);
+    }
 
-        if (authorList.isEmpty())
+    private ResponseEntity<List<AuthorDto>> createResponseEntity(List<AuthorDto> list) {
+        if (list.isEmpty())
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         else
-            return new ResponseEntity<>(authorList, HttpStatus.OK);
-
-        // throws 500:internal server errors due to non-case-sensitivity in search
-        // if there are entries for (e.g.) udk and UDK db will return both which cannot be saved to an optional<>
-//        Optional<AuthorDto> author = authorService.getAuthorByAlias(alias);
-//        // map()-idiom is used with lists and list-like containers and expects a lambda function
-//        return author.map(
-//                        authorDto -> new ResponseEntity<>(authorDto, HttpStatus.OK)
-//                ).
-//                orElseGet(
-//                        () -> new ResponseEntity<>(HttpStatus.NOT_FOUND)
-//                );
+            return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
 
